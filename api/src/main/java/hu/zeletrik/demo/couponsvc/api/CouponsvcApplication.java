8978d19f9e32809ed7e3789e3b8340496b47c1ba
Patrik Zelena <patrik.zelena@gmail.com>
2021-01-25 07:59:53 +0100

    Init project with deps

---
+++ api/src/main/java/hu/zeletrik/demo/couponsvc/api/CouponsvcApplication.java
@@ -0,0 +1,13 @@
+package hu.zeletrik.demo.couponsvc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponsvcApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/hu/zeletrik/demo/couponsvc/api/CouponsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zeletrik.demo.couponsvc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
